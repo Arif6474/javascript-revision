de0e9236b93d6cd03396ced4c4be80ba8fc00569
Arif6474 <arifulislam64743@gmail.com>
2022-11-21 11:42:41 +0600

    swicth practice

---
+++ switch.js
@@ -0,0 +1,29 @@
+const whatMonth = "june"
+switch (whatMonth) {
+    case 'january':
+        console.log(" It's january");
+        break;
+        case 'february':
+            console.log(" It's february");
+            break;
+            case 'march':
+        console.log(" It's march");
+        break;
+        case 'april':
+        console.log(" It's april");
+        break;
+        case 'may':
+            console.log(" It's may");
+            break;
+            case 'june':
+        console.log(" It's june");
+        break;
+        case 'july':
+        console.log(" It's july");
+        break;
+        
+        
+    default:
+        console.log(" It's december");
+        break;
+}
