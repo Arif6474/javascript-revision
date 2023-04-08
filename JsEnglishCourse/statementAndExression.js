f3de7fe622f723fe092e4a3282056c301174c54b
Arif6474 <arifulislam64743@gmail.com>
2023-04-08 16:32:26 +0600

    statement and exprssion

---
+++ JsEnglishCourse/statementAndExression.js
@@ -0,0 +1,31 @@
+////////////////////////////////////
+// Statements and Expressions
+3 + 4 // Expressions
+1991 // Expressions
+true && false && !false // Expressions
+
+// Statements
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas'; // Statements
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
